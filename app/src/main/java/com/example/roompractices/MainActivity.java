228548c9dcdddb1d6fd4969e56b7c0f1c12efe29
Liao Xueqing <xueqingliao@foxmail.com>
2020-07-25 13:53:26 +0800

    Main and Submit page layout

--- app/src/main/java/com/example/roompractices/MainActivity.java
+++ app/src/main/java/com/example/roompractices/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.roompractices;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button viewBtn = findViewById(R.id.btn_view);
+        Button addBtn = findViewById(R.id.btn_add);
+
+        viewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toViewPage();
+            }
+        });
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toAddPage();
+            }
+        });
+    }
+
+    private void toAddPage() {
+        Intent intent = new Intent(this, SubmitActivity.class);
+        startActivity(intent);
+    }
+
+    private void toViewPage() {
+        Intent intent = new Intent(this, ViewActivity.class);
+        startActivity(intent);
     }
 }
